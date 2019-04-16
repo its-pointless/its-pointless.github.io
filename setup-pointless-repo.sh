3b59da8c70ce1a8cb96c8df5f8baf3b56267ef35
its-pointless <bored@goat.si>
2019-04-16 10:10:15 +1000

    update setup-pointless-repo.sh

--- setup-pointless-repo.sh
+++ setup-pointless-repo.sh
@@ -6,7 +6,11 @@ apt-get  --assume-yes install coreutils gnupg wget
 # Make the sources.list.d directory
 mkdir $PREFIX/etc/apt/sources.list.d
 # Write the needed source file
+if apt-cache policy | grep -q "https://dl.bintray.com/termux/termux-packages-24" ; then
+echo "deb https://its-pointless.github.io/files/24 termux extras" > $PREFIX/etc/apt/sources.list.d/pointless.list
+else
 echo "deb https://its-pointless.github.io/files/ termux extras" > $PREFIX/etc/apt/sources.list.d/pointless.list
+fi
 # Download signing key from https://its-pointless.github.io/pointless.gpg 
 wget https://its-pointless.github.io/pointless.gpg
 apt-key add pointless.gpg
