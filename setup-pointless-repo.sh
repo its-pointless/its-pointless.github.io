ebddc3a34b0d1229756c0d6e514e31921314bf69
joshcangit <33022160+joshcangit@users.noreply.github.com>
2020-03-08 16:51:40 +0000

    Update setup-pointless-repo.sh
    Added new condition to the grep command Termux. The website, termux.org, is down.
    https://wiki.termux.com/wiki/Bintray_package_hosting

--- setup-pointless-repo.sh
+++ setup-pointless-repo.sh
@@ -6,7 +6,7 @@ apt-get --assume-yes install coreutils gnupg
 # Make the sources.list.d directory
 mkdir -p $PREFIX/etc/apt/sources.list.d
 # Write the needed source file
-if apt-cache policy | grep -q "https://termux.org/packages" ; then
+if apt-cache policy | grep -q "termux.*24\|termux.org" ; then
 echo "deb https://its-pointless.github.io/files/24 termux extras" > $PREFIX/etc/apt/sources.list.d/pointless.list
 else
 echo "deb https://its-pointless.github.io/files/ termux extras" > $PREFIX/etc/apt/sources.list.d/pointless.list
