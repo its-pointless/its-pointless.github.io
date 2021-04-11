bdbeeb92af94e46973121b00d0ac57a1319781e8
Lucy Phipps <landfillbaby69@gmail.com>
2021-04-12 00:57:24 +0100

    add IPFS detection, fix #74 (#80)
    * add IPFS detection, fix #74
    
    * serverside dearmor

--- setup-pointless-repo.sh
+++ setup-pointless-repo.sh
@@ -6,16 +6,16 @@ apt-get --assume-yes install coreutils gnupg
 # Make the sources.list.d directory
 mkdir -p $PREFIX/etc/apt/sources.list.d
 # Write the needed source file
-if apt-cache policy | grep -q "termux.*24\|termux.org\|bintray.*24" ; then
+if apt-cache policy | grep -q "termux.*24\|termux.org\|bintray.*24\|k51qzi5uqu5dg9vawh923wejqffxiu9bhqlze5f508msk0h7ylpac27fdgaskx" ; then
 echo "deb https://its-pointless.github.io/files/24 termux extras" > $PREFIX/etc/apt/sources.list.d/pointless.list
 else
 echo "deb https://its-pointless.github.io/files/21 termux extras" > $PREFIX/etc/apt/sources.list.d/pointless.list
 fi
 # Add signing key from https://its-pointless.github.io/pointless.gpg
 if [ -n $(command -v curl) ]; then
-curl -sL https://its-pointless.github.io/pointless.gpg | apt-key add -
+curl -sLo $PREFIX/etc/apt/trusted.gpg.d/pointless.gpg --create-dirs https://its-pointless.github.io/pointless.gpg
 elif [ -n $(command -v wget) ]; then
-wget -qO - https://its-pointless.github.io/pointless.gpg | apt-key add -
+wget -qP $PREFIX/etc/apt/trusted.gpg.d https://its-pointless.github.io/pointless.gpg
 fi
 # Update apt
 apt update
